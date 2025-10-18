b8d9d7409309463009014c156c79a3563fc1a317
Shmelev Valentin <shmelevvl@mail.ru>
2025-10-18 11:21:24 +0700

    настроил работу с БД

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './config/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+  ],
 })
 export class AppModule {}

b2dc2829a7e2edf4872307209a94ba9f12fac556
Shmelev Valentin <shmelevvl@mail.ru>
2025-11-26 11:43:49 +0700

    Наброски мониторинга

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Registry, collectDefaultMetrics } from 'prom-client';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,15 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  // Prometheus metrics
+  const register = new Registry();
+  collectDefaultMetrics({ register });
+  const httpAdapter = app.getHttpAdapter();
+  httpAdapter.get('/metrics', async (req, res) => {
+    res.header('Content-Type', register.contentType);
+    res.send(await register.metrics());
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
